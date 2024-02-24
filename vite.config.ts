01de7ac285a776c3ce1fbd5c0a92b25136330c98
Jhonatan Henkel <jhowrf@gmail.com>
2024-02-24 12:50:57 -0300

    build: define env file andd configure vite to load this env

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
 import legacy from '@vitejs/plugin-legacy'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  }
+export default defineConfig(() => {
+    const env = loadEnv('', process.cwd())
+    return {
+        plugins: [
+            vue(),
+            legacy(),
+        ],
+        envDir: './src/environments/.env',
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+        define: {
+            'process.env': env
+        },
+        test: {
+            globals: true,
+            environment: 'jsdom'
+        }
+    }
 })
