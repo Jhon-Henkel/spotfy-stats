15a4962ba6de44873c3a0060e0fa4a942df6e6be
Jhonatan Henkel <jhowrf@gmail.com>
2024-02-24 21:15:37 -0300

    feat: create component to connection account status

---
+++ src/components/status/SyncStatus.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {IonRow, IonIcon, IonText, IonCol} from "@ionic/vue";
+import { SpotifyAuthStore } from "@/services/spotify/auth/stores/authStore";
+import {ref} from "vue";
+
+const store = SpotifyAuthStore()
+const isSync: boolean = !!ref(store.accessToken).value
+
+</script>
+
+<template>
+    <ion-row>
+        <ion-col>
+            <ion-text class="status-component">
+                Sincronizado:
+                <ion-icon :name="isSync ? 'checkmark-circle-outline' : 'close-circle-outline'"
+                          :color="isSync ? 'success' : 'danger'"
+                          size="small"
+                          class="ion-padding-start"/>
+            </ion-text>
+        </ion-col>
+    </ion-row>
+</template>
+
+<style scoped>
+.status-component {
+    display: flex;
+    align-items: center;
+}
+</style>
