29b10f4f04868f096fde9e6349622fe72954e616
Jhonatan Henkel <jhowrf@gmail.com>
2024-02-24 21:17:39 -0300

    feat: organized index.html and validate connection status on app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
-    <ion-app>
-        <!-- todo se não tiver conectado, deve redirecionar para a tela de conexão -->
-        <!-- todo alterar o favicon -->
-        <!-- todo alterar o nome na guia do navegador -->
-        <!-- todo background no tema do Spotify -->
+    <!-- todo se não tiver conectado, deve redirecionar para a tela de conexão -->
+    <!-- todo alterar o favicon -->
+    <!-- todo alterar o nome na guia do navegador -->
+    <!-- todo background no tema do Spotify -->
+    <ion-app class="main-content">
         <ion-content>
-            <ion-split-pane content-id="main-content">
-                <ion-router-outlet id="main-content"></ion-router-outlet>
-            </ion-split-pane>
+            <ion-router-outlet></ion-router-outlet>
         </ion-content>
     </ion-app>
 </template>
 
 <script setup lang="ts">
-import {IonApp, IonRouterOutlet, IonSplitPane, IonContent} from '@ionic/vue';
+import {IonApp, IonRouterOutlet, IonContent} from '@ionic/vue';
+import {ref} from "vue";
+import {SpotifyAuthStore} from "@/services/spotify/auth/stores/authStore";
+import router from "@/router";
 
+const store = SpotifyAuthStore()
+const isSync: boolean = !!ref(store.accessToken).value
+
+if (! isSync) {
+    router.push({ name: 'SyncAccount' })
+}
 </script>
 
 <style scoped>
-
+.main-content {
+    height: 100%;
+    width: 100%;
+}
 </style>
