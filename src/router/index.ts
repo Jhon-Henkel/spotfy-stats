ce166e8586c2bf2a71bd1929553c1c9121f58a52
Jhonatan Henkel <jhowrf@gmail.com>
2024-02-24 21:18:00 -0300

    feat: create home route and base component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,24 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
+import SyncAccount from "@/views/syncAccount/SyncAccount.vue";
+import Home from "@/views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '',
-    redirect: '/folder/Inbox'
-  },
-  {
-    path: '/sincronizar-conta',
-    component: () => import ('../views/syncAccount/SyncAccount.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/sincronizar-conta',
+        name: 'SyncAccount',
+        component: SyncAccount
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
 })
 
 export default router
