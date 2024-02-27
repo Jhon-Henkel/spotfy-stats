3e95033f23444375faba2dd3678c2547b0f29ec5
Jhonatan Henkel <jhowrf@gmail.com>
2024-02-27 20:48:17 -0300

    feat: desenvolvido footer

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,19 @@
                 </template>
             </suspense>
         </ion-content>
+        <ion-footer>
+            <ion-toolbar>
+                <ion-title>
+                    Desenvolvido por
+                    <a href="https://jhon.dev.br" target="_blank" class="footer-link">Jhon</a>
+                </ion-title>
+            </ion-toolbar>
+        </ion-footer>
     </ion-app>
-    <!-- todo criar footer -->
 </template>
 
 <script setup lang="ts">
-import {IonApp, IonContent} from '@ionic/vue';
+import {IonApp, IonContent, IonTitle, IonToolbar, IonFooter} from '@ionic/vue';
 import router from "@/router";
 import SpotifyAuthService from "@/services/spotify/auth/authService";
 import AppHomeHeader from "@/components/header/AppHomeHeader.vue";
@@ -34,5 +41,9 @@ if (service.mustRequestSpotifyLogin()) {
 .main-content {
     height: 100%;
     width: 100%;
+}
+.footer-link {
+    color: var(--ion-color-success);
+    text-decoration: none;
 }
 </style>
