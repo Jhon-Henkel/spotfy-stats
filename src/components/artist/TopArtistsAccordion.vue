9890b2b773cef4e59ebf4d08ff39edd64d5035de
Jhonatan Henkel <jhowrf@gmail.com>
2024-02-26 21:02:22 -0300

    feat: create top artists page

---
+++ src/components/artist/TopArtistsAccordion.vue
@@ -0,0 +1,121 @@
+<script lang="ts">
+
+import {
+    IonAccordion,
+    IonBadge,
+    IonCol,
+    IonGrid,
+    IonIcon,
+    IonItem,
+    IonItemDivider,
+    IonLabel,
+    IonRow,
+    IonText
+} from "@ionic/vue";
+
+export default {
+    components: {
+        IonAccordion,
+        IonIcon,
+        IonItem,
+        IonBadge,
+        IonText,
+        IonLabel,
+        IonItemDivider,
+        IonRow,
+        IonGrid,
+        IonCol
+    },
+    props: {
+        artists: {
+            type: Array,
+            required: true
+        },
+        label: {
+            type: String,
+            required: true
+        },
+        position: {
+            type: String,
+            required: true
+        }
+    },
+    setup() {
+        const defaultImage = 'https://ionicframework.com/docs/img/demos/card-media.png'
+        return {
+            defaultImage
+        }
+    }
+}
+</script>
+
+<template>
+    <ion-accordion :value="position">
+        <ion-item slot="header" color="success">
+            <ion-label>{{ label }}</ion-label>
+        </ion-item>
+        <div class="ion-padding" slot="content">
+            <ion-item-divider v-for="(artist, index) in artists" :key="index">
+                <ion-grid>
+                    <ion-row class="ion-text-start">
+                        <ion-col size="1" class="position-col">
+                            <ion-badge color="success" class="position-badge">
+                                {{ (index + 1) }}ª
+                            </ion-badge>
+                        </ion-col>
+                        <ion-col size="1">
+                            <img :src="artist?.images[0]?.url ?? defaultImage"
+                                 alt="album cover">
+                        </ion-col>
+                        <ion-col size="9">
+                            <ion-grid>
+                                <ion-row>
+                                    <ion-col>
+                                        <ion-label class="track-name">
+                                            {{ artist?.name ?? 'Desconhecido' }}
+                                        </ion-label>
+                                    </ion-col>
+                                </ion-row>
+                                <ion-row>
+                                    <ion-col>
+                                        <ion-text>
+                                            Seguidores: {{ artist?.followers?.total ?? 'Desconhecido' }}
+                                        </ion-text>
+                                    </ion-col>
+                                </ion-row>
+                            </ion-grid>
+                        </ion-col>
+                        <ion-col size="1" class="position-col">
+                            <a :href="artist?.external_urls?.spotify ?? 'https://open.spotify.com/'"
+                               target="_blank"
+                               class="play-icon">
+                                <ion-icon name="play-circle-outline" color="success"/>
+                            </a>
+                        </ion-col>
+                    </ion-row>
+                </ion-grid>
+            </ion-item-divider>
+        </div>
+    </ion-accordion>
+</template>
+
+<style scoped>
+.position-col {
+    display: flex;
+    align-items: center;
+}
+.position-badge {
+    --padding-end: 20%;
+    --padding-start: 20%;
+    --padding-top: 10%;
+    --padding-bottom: 10%;
+}
+.track-name {
+    font-size: 1.2em;
+    font-weight: 700;
+    color: #ffffff;
+}
+.play-icon {
+    font-size: 3em;
+}
+</style>
--- src/views/topItems/TopArtistsView.vue
+++ src/views/topItems/TopArtistsView.vue
@@ -1,34 +1,45 @@
 <script lang="ts">
 import SpotifyTopItemsService from "@/services/spotify/topItems/topItemsService";
+import AppPagesHeader from "@/components/header/AppPagesHeader.vue";
+import {
+    IonAccordionGroup,
+    IonCol,
+    IonGrid,
+    IonRow,
+} from "@ionic/vue";
+import TopArtistsAccordion from "@/components/artist/TopArtistsAccordion.vue";
 
 export default {
+    components: {
+        TopArtistsAccordion,
+        IonAccordionGroup, IonRow, IonGrid, IonCol, AppPagesHeader},
     async setup() {
         const topItems = new SpotifyTopItemsService()
         const topArtistsAllTime = await topItems.getTopArtistsAllTime()
         const topArtistsLastSixMonths = await topItems.getTopArtistsLastSixMonths()
         const topArtistsLastFourWeeks = await topItems.getTopArtistsLastFourWeeks()
+
         return {
             topArtistsAllTime: topArtistsAllTime,
             topArtistsLastSixMonths: topArtistsLastSixMonths,
-            topArtistsLastFourWeeks: topArtistsLastFourWeeks
+            topArtistsLastFourWeeks: topArtistsLastFourWeeks,
         }
     }
 }
 </script>
 
 <template>
-    <div>
-        <h1>Top Faixas (all time)</h1>
-        <div v-for="(artist, index) in topArtistsAllTime" :key="index">
-            Faixa {{ (index + 1) }}: {{ artist.name }}
-        </div>
-        <h1>Top Faixas (six months)</h1>
-        <div v-for="(artist, index) in topArtistsLastSixMonths" :key="index">
-            Faixa {{ (index + 1) }}: {{ artist.name }}
-        </div>
-        <h1>Top Faixas (four weeks)</h1>
-        <div v-for="(artist, index) in topArtistsLastFourWeeks" :key="index">
-            Faixa {{ (index + 1) }}: {{ artist.name }}
-        </div>
-    </div>
+    <app-pages-header>Top Artistas</app-pages-header>
+    <ion-grid>
+        <ion-row>
+            <ion-col size="1" />
+            <ion-col size="10" class="ion-text-center">
+                <ion-accordion-group expand="inset">
+                    <top-artists-accordion position="first" label="Desde o início" :artists="topArtistsAllTime" />
+                    <top-artists-accordion position="second" label="Últimos 6 meses" :artists="topArtistsLastSixMonths" />
+                    <top-artists-accordion position="third" label="Últimas 4 semanas" :artists="topArtistsLastFourWeeks" />
+                </ion-accordion-group>
+            </ion-col>
+        </ion-row>
+    </ion-grid>
 </template>
